9224580af128b7585acb0fdb4efe07eaab9a9a81
zhaoyangkanshijie <2191732408@qq.com>
2020-08-13 15:05:25 +0800

    uniappExample

---
+++ pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<text>1</text>
+		<navigator url="/pages/about/about">2222222</navigator>
+		<view>211111111111</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		},
+		onShow() {
+			console.log('onShow')
+		},
+		onPullDownRefresh() {
+			console.log('onPullDownRefresh')
+		},
+		beforeCreate() {
+			console.log('beforeCreate')
+		},
+		mounted() {
+			console.log('mounted',getApp(),getApp().globalData,getCurrentPages(),this.$scope)
+			// #ifdef APP-PLUS
+			    console.log(this.$scope.$getAppWebview())
+			// #endif
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
